a7c5eaed369d2fa6ed15b6360cf0ccbce51966f0
Oriol Torras <uri.torras@gmail.com>
2011-09-13 16:41:41 +0200

    reorganized function specs suites

--- test/spec/core/function.js
+++ test/spec/core/function.js
@@ -1,15 +1,14 @@
 describe('Function', function () {
+  describe('bind method', function () {
+    it('can bind `this` function context to an object', function () {
+      var objectToInject = {
+        aProperty : 1
+      };      
+      var fun = function () {
+        return this.aProperty;
+      }.bind(objectToInject);
 
-  it('can bind objects to functions', function () {
-    var objectToInject = {
-      aProperty : 1
-    };
-    
-    var fun = function () {
-      return this.aProperty;
-    }.bind(objectToInject);
-
-    expect(fun()).toEqual(objectToInject.aProperty);
+      expect(fun()).toEqual(objectToInject.aProperty);
+    });
   });
-
 });
