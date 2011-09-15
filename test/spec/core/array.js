49de18b0f8272f0e4433e36476456f8d3aa13412
Oriol Torras <uri.torras@gmail.com>
2011-09-15 23:56:28 +0200

    updated array spec

--- test/spec/core/array.js
+++ test/spec/core/array.js
@@ -1,61 +1,81 @@
-﻿describe('Array.prototype', function(){
-  var methodPrototypeMessage = 'should be implemented';
-	var methodPrototypeFunction = function(nativeObject, methodName){
-    expect(typeof nativeObject.prototype[methodName]).toEqual('function');
-  };
+﻿describe('Array', function(){
+  describe('prototype', function(){
+    var methodPrototypeMessage = 'should be implemented';
+	  var methodPrototypeFunction = function(nativeObject, methodName){
+      expect(typeof nativeObject.prototype[methodName]).toEqual('function');
+    };
   
-  describe('indexOf', function(){
-    it(methodPrototypeMessage, function () {
-      methodPrototypeFunction(Array, 'indexOf');
+    describe('indexOf', function(){
+      it(methodPrototypeMessage, function () {
+        methodPrototypeFunction(Array, 'indexOf');
+      });
+    });
+  
+    describe('lastIndexOf', function(){
+      it(methodPrototypeMessage, function () {
+        methodPrototypeFunction(Array, 'lastIndexOf');
+      });
+    });
+  
+    describe('every', function(){
+      it(methodPrototypeMessage, function () {
+        methodPrototypeFunction(Array, 'every');
+      });
+    });
+  
+    describe('some', function(){
+      it(methodPrototypeMessage, function () {
+        methodPrototypeFunction(Array, 'some');
+      });
+    });
+  
+    describe('forEach', function(){
+      it(methodPrototypeMessage, function () {
+        methodPrototypeFunction(Array, 'forEach');
+      });
+    });
+  
+    describe('map', function(){
+      it(methodPrototypeMessage, function () {
+        methodPrototypeFunction(Array, 'map');
+      });
+    });
+  
+    describe('filter', function(){
+      it(methodPrototypeMessage, function () {
+        methodPrototypeFunction(Array, 'filter');
+      });
+    });
+  
+    describe('reduce', function(){
+      it(methodPrototypeMessage, function () {
+        methodPrototypeFunction(Array, 'reduce');
+      });
+    });
+  
+    describe('reduceRight', function(){
+      it(methodPrototypeMessage, function () {
+        methodPrototypeFunction(Array, 'reduceRight');
+      });
     });
   });
-  
-  describe('lastIndexOf', function(){
-    it(methodPrototypeMessage, function () {
-      methodPrototypeFunction(Array, 'lastIndexOf');
+
+  describe('isArray', function(){
+    it('should be implemented', function(){
+      expect(typeof Array.isArray).toEqual('function');
+    });
+    it('should return true when an Array object is passed as argument', function(){
+      expect(Array.isArray([])).toBeTruthy();
+      expect(Array.isArray(new Array)).toBeTruthy();
+      expect(Array.isArray(Array())).toBeTruthy();
+    });
+    it('should return false otherwise', function(){
+      expect(Array.isArray()).toBeFalsy();
+      expect(Array.isArray({})).toBeFalsy();
+      expect(Array.isArray('')).toBeFalsy();
+      expect(Array.isArray(0)).toBeFalsy();
+      expect(Array.isArray(function(){})).toBeFalsy();
     });
   });
-  
-  describe('every', function(){
-    it(methodPrototypeMessage, function () {
-      methodPrototypeFunction(Array, 'every');
-    });
-  });
-  
-  describe('some', function(){
-    it(methodPrototypeMessage, function () {
-      methodPrototypeFunction(Array, 'some');
-    });
-  });
-  
-  describe('forEach', function(){
-    it(methodPrototypeMessage, function () {
-      methodPrototypeFunction(Array, 'forEach');
-    });
-  });
-  
-  describe('map', function(){
-    it(methodPrototypeMessage, function () {
-      methodPrototypeFunction(Array, 'map');
-    });
-  });
-  
-  describe('filter', function(){
-    it(methodPrototypeMessage, function () {
-      methodPrototypeFunction(Array, 'filter');
-    });
-  });
-  
-  describe('reduce', function(){
-    it(methodPrototypeMessage, function () {
-      methodPrototypeFunction(Array, 'reduce');
-    });
-  });
-  
-  describe('reduceRight', function(){
-    it(methodPrototypeMessage, function () {
-      methodPrototypeFunction(Array, 'reduceRight');
-    });
-  });
-  
+
 });
