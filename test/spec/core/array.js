9367a3dd506bce9c8005f86472e70d39bd075469
Oriol Torras <uri.torras@gmail.com>
2011-09-15 17:34:56 +0200

    added array prototype spec

---
+++ test/spec/core/array.js
@@ -0,0 +1,61 @@
+﻿describe('Array.prototype', function(){
+  var methodPrototypeMessage = 'should be implemented';
+	var methodPrototypeFunction = function(nativeObject, methodName){
+    expect(typeof nativeObject.prototype[methodName]).toEqual('function');
+  };
+  
+  describe('indexOf', function(){
+    it(methodPrototypeMessage, function () {
+      methodPrototypeFunction(Array, 'indexOf');
+    });
+  });
+  
+  describe('lastIndexOf', function(){
+    it(methodPrototypeMessage, function () {
+      methodPrototypeFunction(Array, 'lastIndexOf');
+    });
+  });
+  
+  describe('every', function(){
+    it(methodPrototypeMessage, function () {
+      methodPrototypeFunction(Array, 'every');
+    });
+  });
+  
+  describe('some', function(){
+    it(methodPrototypeMessage, function () {
+      methodPrototypeFunction(Array, 'some');
+    });
+  });
+  
+  describe('forEach', function(){
+    it(methodPrototypeMessage, function () {
+      methodPrototypeFunction(Array, 'forEach');
+    });
+  });
+  
+  describe('map', function(){
+    it(methodPrototypeMessage, function () {
+      methodPrototypeFunction(Array, 'map');
+    });
+  });
+  
+  describe('filter', function(){
+    it(methodPrototypeMessage, function () {
+      methodPrototypeFunction(Array, 'filter');
+    });
+  });
+  
+  describe('reduce', function(){
+    it(methodPrototypeMessage, function () {
+      methodPrototypeFunction(Array, 'reduce');
+    });
+  });
+  
+  describe('reduceRight', function(){
+    it(methodPrototypeMessage, function () {
+      methodPrototypeFunction(Array, 'reduceRight');
+    });
+  });
+  
+});
