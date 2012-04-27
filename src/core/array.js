cd3a5875d9728d42a0e63ee9d2c3ee7c9d1e012e
Oriol Torras <uri.torras@gmail.com>
2012-04-28 01:53:23 +0200

    array refactoring

--- src/core/array.js
+++ src/core/array.js
@@ -1,112 +1,93 @@
 (function(ArrayProto) {
-	if (!ArrayProto.indexOf) {
+	if (!ArrayProto.indexOf)
 		ArrayProto.indexOf = function(searchElement /*, fromIndex */ ) {
 			var len = this.length;
-			if (len === 0) {
+			if (len === 0)
 				return -1;
-			}
+
 			var n = len;
 			if (arguments.length > 0) {
 				n = Number(arguments[1]);
-				if (n != n) { // short for isNan
+				if (n != n) // short for isNan
 					n = 0;
-				}
 			}
-			if (n >= len) {
+			if (n >= len)
 				return -1;
-			}
-			var k = n < 0 ? len - Math.abs(n) : n;
-			for (; k < len; k++) {
-				if (k in this && this[k] === searchElement) {
-					return k;
-				}
-			}
+
+			var i = n < 0 ? len - Math.abs(n) : n;
+			for (; i < len; i++)
+				if (i in this && this[i] === searchElement)
+					return i;
 
 			return -1;
 		};
-	}
 
-	if (!ArrayProto.lastIndexOf) {
+	if (!ArrayProto.lastIndexOf)
 		ArrayProto.lastIndexOf = function(searchElement /*, fromIndex */ ) {
 			var len = this.length;
-
-			if (len === 0) {
-				return -1;
-			}
+			if (len === 0)
+			 return -1;
+			
 			var n = len;
 			if (arguments.length > 0) {
 				n = Number(arguments[1]);
-				if (n != n) { // short for isNan
+				if (n != n) // short for isNan
 					n = 0;
-				}
 			}
-			var k = n > 0 ? Math.min(n, len - 1) : len - Math.abs(n);
-			for (; k >= 0; k--) {
-				if (k in this && this[k] === searchElement) {
-					return k;
-				}
-			}
+
+			var i = n > 0 ? Math.min(n, len - 1) : len - Math.abs(n);
+			for (; i >= 0; i--)
+				if (i in this && this[i] === searchElement)
+					return i;
 
 			return -1;
-		}
-	}
+		};
 
-	if (!ArrayProto.every) {
+	if (!ArrayProto.every)
 		ArrayProto.every = function(callbackfn /*, thisArg */ ) {
 			var len = this.length;
 
-			if ("function" !== typeof callbackfn) {
-				throw new TypeError(callbackfn + " is not a function");
-			}
+			if (typeof callbackfn !== 'function')
+				throw new TypeError(callbackfn + ' is not a function');
 
 			var thisArg = arguments[1];
-			for (var i = 0; i < len; i++) {
-				if (i in this && !callbackfn.call(thisArg, this[i], i, this)) {
+			for (var i = 0; i < len; i++)
+				if (i in this && !callbackfn.call(thisArg, this[i], i, this))
 					return false;
-				}
-			}
 
 			return true;
 		};
-	}
 
-	if (!ArrayProto.some) {
+	if (!ArrayProto.some)
 		ArrayProto.some = function(callbackfn /*, thisArg*/ ) {
 			var len = this.length;
 
-			if ("function" !== typeof callbackfn) {
-				throw new TypeError(callbackfn + " is not a function");
-			}
+			if (typeof callbackfn !== 'function')
+				throw new TypeError(callbackfn + ' is not a function');
 
 			var thisArg = arguments[1];
-			for (var i = 0; i < len; i++) {
-				if (i in this && callbackfn.call(thisArg, this[i], i, this)) {
+			for (var i = 0; i < len; i++)
+				if (i in this && callbackfn.call(thisArg, this[i], i, this))
 					return true;
-				}
-			}
 
 			return false;
 		};
-	}
 
-	if (!ArrayProto.forEach) {
+	if (!ArrayProto.forEach)
 		ArrayProto.forEach = function(callbackfn /*, thisArg*/ ) {
 			var len = this.length;
 
-			if ("function" !== typeof callbackfn) {
-				throw new TypeError(callbackfn + " is not a function");
-			}
+			if (typeof callbackfn !== 'function')
+				throw new TypeError(callbackfn + ' is not a function');
 
 			var thisArg = arguments[1];
-			for (var i = 0; i < len; i++) {
-				if (i in this) callbackfn.call(thisArg, this[i], i, this);
-			}
+			for (var i = 0; i < len; i++)
+				if (i in this) 
+					callbackfn.call(thisArg, this[i], i, this);
 		};
-	}
 })(Array.prototype);
 
-if (typeof Array.isArray !== 'function') {
+if (typeof Array.isArray !== 'function')
 	Array.isArray = function(val) {
 		return Object.prototype.toString.apply(val) === '[object Array]';
 	};
-}
