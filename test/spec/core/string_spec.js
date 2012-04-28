71a45379b3762e4a8cdb750bac2a34528ca2fb56
Oriol Torras <uri.torras@gmail.com>
2012-04-28 03:54:59 +0200

    coding style

--- test/spec/core/string_spec.js
+++ test/spec/core/string_spec.js
@@ -1,11 +1,11 @@
-﻿describe('String', function () {
-  describe('trim method', function () {
-    it('should be implemented in the prototype object', function () {
+﻿describe('String', function() {
+  describe('trim method', function() {
+    it('should be implemented in the prototype object', function() {
       var trim = String.prototype.trim;
       expect(typeof trim).toEqual('function');
     });
 
-    it('should remove begining and end spaces of the string', function () {
+    it('should remove begining and end spaces of the string', function() {
       var testString = "         I must be trimmed      ";
       expect(testString.trim()).toEqual("I must be trimmed");
     });
